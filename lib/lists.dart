6d2d38bdf9d8721fcf9c6546d7d0733e1c36c11a
taka-2120 <yu_tkhs@icloud.com>
2022-05-15 01:27:39 +0900

    add, style: Subs List

---
+++ lib/lists.dart
@@ -0,0 +1,18 @@
+import 'dart:ffi';
+import 'package:flutter/widgets.dart';
+
+class SubsList {
+  String name;
+  double fee;
+  // String url;
+  // String period;
+  // DateTime date;
+
+  SubsList({
+    required this.name,
+    required this.fee,
+    // required this.url,
+    // required this.period,
+    // required this.date,
+  });
+}
--- lib/pages/subs.dart
+++ lib/pages/subs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:subsmanager/lists.dart';
 import '../globals.dart' as globals;
 
 class Subs extends StatelessWidget {
@@ -18,8 +19,12 @@ class SubsPage extends StatefulWidget {
 }
 
 class _SubsState extends State<SubsPage> {
+  List<SubsList> subsList = [SubsList(name: "Apple One", fee: 1030.toDouble())];
+
   void _add() {
-    setState(() {});
+    setState(() {
+      subsList.add(SubsList(name: "Apple One", fee: 1030.toDouble()));
+    });
   }
 
   @override
@@ -28,24 +33,55 @@ class _SubsState extends State<SubsPage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(15),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                Text(globals.subs,
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-              ],
-            ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Text(globals.subs,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    return _subsItem(subsList[index].name, subsList[index].fee);
+                  },
+                  itemCount: subsList.length,
+                ),
+              ),
+            ],
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _add,
-        tooltip: 'Increment',
+        tooltip: 'Add a Subscriptiom',
         child: const Icon(Icons.add),
       ),
     );
   }
+}
+
+Widget _subsItem(String name, double fee) {
+  return Container(
+    margin: const EdgeInsets.all(10),
+    padding: const EdgeInsets.all(10),
+    height: 100,
+    decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(15)),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          name,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+        ),
+        Text(
+          fee.toString(),
+          style: const TextStyle(
+              fontSize: 18, color: Color.fromARGB(255, 104, 104, 104)),
+        ),
+      ],
+    ),
+  );
 }
